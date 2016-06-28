b1339e69186df2b0931edc1793391a632a848d5d
jackie19 <jackie198512@126.com>
2016-06-28 16:04:18 +0800

    Initial commit

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/6/27.
+ */
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+bootstrap(AppComponent);
